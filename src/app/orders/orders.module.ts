82ceb86a479b6fdb2f5334016255b44d421772eb
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2021-02-08 19:47:08 +0530

    list order component

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListOrderComponent } from './list-order/list-order.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListOrderComponent],
   imports: [
     CommonModule
   ]
